fa38118d9be1b29a1fd2b800628f59590b7373b0
Joao Pucci <joao.pucci@live.com>
2017-02-26 15:45:20 -0300

    Teste + Criação da branch

--- app/src/main/java/com/example/ruanstaron/mercadinho/MainActivity.java
+++ app/src/main/java/com/example/ruanstaron/mercadinho/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+//teste
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button scanBtn;
