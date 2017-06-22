aec2b82dcbe6c58e95066cc9021bb081c393fba0
Ruan Staron <ruan.staron@hotmail.com>
2017-06-21 23:46:42 -0300

    - Método Post funcionando

--- app/src/main/java/com/example/ruanstaron/mercadinho/MainActivity.java
+++ app/src/main/java/com/example/ruanstaron/mercadinho/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.ruanstaron.mercadinho;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ruanstaron.mercadinho.db.BdDefinitivo;
@@ -14,8 +15,16 @@ import com.example.ruanstaron.mercadinho.db.DaoMaster;
 import com.example.ruanstaron.mercadinho.db.DaoSession;
 
 import org.greenrobot.greendao.database.Database;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
-import java.util.ArrayList;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -30,10 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     private final String WS_SERVER = "http://mercadinho.96.lt/";
     private final String WS_SERVER_TESTE = "http://10.0.2.2/";
+    private final String WS_SERVER_POST = "http://mercadinho.96.lt/index.php/addprodutos";
 
 
     //ArrayList<BdDefinitivo> bdd = new ArrayList<>();
     //BdDefinitivo hue = new BdDefinitivo();
+    TextView json = (TextView) findViewById(R.id.json);
 
 
 
@@ -42,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        //Para poder conectar na net
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        postProdutos();
 
         //List<BdDefinitivo> lBdDefinitivo;
         // bddTeste.setCod_barras("7896541230");
@@ -49,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
+    /*@Override
     protected void onStart() {
         super.onStart();
        // bddTeste.setCod_barras("7896541230");
@@ -137,5 +153,62 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Erro Inserindo", Toast.LENGTH_SHORT).show();
             }
         });
+    }*/
+
+    public void postProdutos(){
+        JSONObject produto = new JSONObject();
+        JSONObject produto2 = new JSONObject();
+        JSONObject prontoEnvio = new JSONObject();
+        JSONArray produtos = new JSONArray();
+
+        try {
+            produto.put("cod_barras", "20658");
+            produto.put("descricao", "teste_json");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        produtos.put(produto);
+
+        try {
+            produto2.put("cod_barras", "2424");
+            produto2.put("descricao", "joao_viadao");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        produtos.put(produto2);
+        try {
+            prontoEnvio.put("produto",produtos);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        json.setText(prontoEnvio.toString());
+        //-------------------------------------//
+
+        String requestJson = prontoEnvio.toString();
+
+        try {
+            URL url = new URL("http://mercadinho.96.lt/index.php/addprodutos");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(15000);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Length", Integer.toString(requestJson.length()));
+            DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+            stream.write(requestJson.getBytes("UTF-8"));
+            stream.flush();
+            stream.close();
+            connection.connect();
+
+            String responseJson = connection.getInputStream().toString();
+            connection.disconnect();
+            Toast.makeText(MainActivity.this, responseJson, Toast.LENGTH_SHORT).show();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
