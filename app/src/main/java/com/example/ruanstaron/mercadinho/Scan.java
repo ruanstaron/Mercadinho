97f34479ca6f3720a183ac19f47bb14b91c67c66
Joao Pucci <joao.pucci@live.com>
2017-08-01 00:31:23 -0300

    - Incluída opção de inserção de lista a partir da tela de compras usando DialogFragments. - Refatoramentos menores.

--- app/src/main/java/com/example/ruanstaron/mercadinho/Scan.java
+++ app/src/main/java/com/example/ruanstaron/mercadinho/Scan.java
@@ -1,5 +1,7 @@
 package com.example.ruanstaron.mercadinho;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.ruanstaron.mercadinho.db.BdDefinitivo;
@@ -12,13 +14,19 @@ import com.example.ruanstaron.mercadinho.db.Produtos;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import android.content.Intent;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.support.v7.app.AlertDialog;
+import android.support.v4.app.DialogFragment;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +38,7 @@ public class Scan extends AppCompatActivity implements OnClickListener {
     private ListView listaProdutos;
     private ArrayList<String> produtos;
     private ArrayAdapter<String> arrayAdapter;
+    private ImageButton btnConfirma;
     DaoMaster.DevOpenHelper helper;
     DaoMaster master;
     DaoSession session;
@@ -43,7 +52,6 @@ public class Scan extends AppCompatActivity implements OnClickListener {
         helper = new DaoMaster.DevOpenHelper(this, "mercadinho-db");
         master = new DaoMaster(helper.getWritableDatabase());
         session = master.newSession();
-        geraLista(session);
         scanBtn = (Button)findViewById(R.id.scan_button);
         okBtn = (Button)findViewById(R.id.bOk);
         etProduto = (EditText) findViewById(R.id.etProduto);
@@ -54,19 +62,27 @@ public class Scan extends AppCompatActivity implements OnClickListener {
         scanBtn.setOnClickListener(this);
         okBtn.setOnClickListener(this);
         produtos = new ArrayList<String>();
+        btnConfirma = (ImageButton) findViewById(R.id.btnAddLista);
+        btnConfirma.setOnClickListener(this);
     }
 
     public void onClick(View v){
-        if(v.getId()==R.id.scan_button){
-            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-            scanIntegrator.initiateScan();
+        switch (v.getId()){
+            case R.id.btnAddLista:
+                DialogoConfirmaLista dfConfirmaLista = new DialogoConfirmaLista();
+                dfConfirmaLista.show(getSupportFragmentManager(), "dfConfirmaLista");
+
+                break;
+            case R.id.scan_button:
+                IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+                scanIntegrator.initiateScan();
+                break;
+            case R.id.bOk:
+                geraProduto();
+                break;
         }
-        if(v.getId()==R.id.bOk){
-            geraProduto(v);
-            etProduto.setText("");
-            etQuantidade.setText("");
-            etValor.setText("");
-        }
+
+        // TODO: IMPLEMENTAR UM LISTENER CUSTOMIZADO OU IMPLEMENTAR DE FORMA ANONIMA
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -85,7 +101,12 @@ public class Scan extends AppCompatActivity implements OnClickListener {
         }
     }
 
-    public void geraProduto(View v){
+    public void geraProduto(){
+        if(etProduto.getText().toString().isEmpty()) {
+            Toast.makeText(this, R.string.geraProdutoPreencherProduto, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         Double valorTotal = Integer.parseInt(etQuantidade.getText().toString()) * Double.parseDouble(etValor.getText().toString());
         Produtos produto = new Produtos(this.cod_barras, etProduto.getText().toString(),
                 Integer.parseInt(etQuantidade.getText().toString()), Double.parseDouble(etValor.getText().toString()), valorTotal);
@@ -94,6 +115,10 @@ public class Scan extends AppCompatActivity implements OnClickListener {
         listaProdutos.setAdapter(arrayAdapter);
         valorTotalCompra = valorTotalCompra+produto.getValorTotal();
         tvValorTotal.setText(valorTotalCompra.toString());
+
+        etProduto.setText("");
+        etQuantidade.setText("");
+        etValor.setText("");
     }
 
     public String verificaProduto(DaoSession session, String cod_barras){
@@ -106,13 +131,64 @@ public class Scan extends AppCompatActivity implements OnClickListener {
         }
     }
 
-    public void geraLista(DaoSession session){
+    public void IncluirBancoLista(DaoSession session, Lista lista){
         listaDao = session.getListaDao();
-        Lista lista = new Lista();
-        lista.setDescricao("teste");
         listaDao.insert(lista);
         Toast toast = Toast.makeText(getApplicationContext(),
                 lista.getId().toString(), Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public static class DialogoConfirmaLista extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialogInterface, int button){
+                    if(button == DialogInterface.BUTTON_POSITIVE){
+                        dismiss();
+                        DialogoNomeLista dfNomeDialogo = new DialogoNomeLista();
+                        dfNomeDialogo.show(getFragmentManager(), "dfNomeDialogo");
+                    }
+                }
+            };
+
+            AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dialogoConfirmaListaTitulo)
+                    .setMessage(R.string.dialogoConfirmaListaMsg)
+                    .setPositiveButton(R.string.sim, listener)
+                    .setNegativeButton(R.string.nao, null)
+                    .create();
+            return dialog;
+        }
+    }
+
+    public static class DialogoNomeLista extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final EditText input = new EditText(getActivity());
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+            DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Scan scan = (Scan)getActivity();
+                    Lista lista = new Lista();
+                    lista.setDescricao(input.getText().toString());
+                    scan.IncluirBancoLista(scan.session, lista);
+                    dismiss();
+                    scan.finish();
+                }
+            };
+
+            AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setView(input)
+                    .setTitle(R.string.dialogoNomeListaTitulo)
+                    .setMessage(R.string.dialogoNomeListaMsg)
+                    .setPositiveButton(R.string.ok, listener)
+                    .create();
+
+                return dialog;
+        }
     }
 }
